a5981dcf2c09d6448e986711ace61cb544df0bf4
lockbeck <t.lochin.98@gmail.com>
2024-09-11 14:20:51 +0500

    Initial commit

--- src/main/java/com/lockbeck/entities/json/JsonController.java
+++ src/main/java/com/lockbeck/entities/json/JsonController.java
@@ -1,15 +1,12 @@
 package com.lockbeck.entities.json;
 
 import com.lockbeck.config.JwtService;
-import com.lockbeck.entities.user.UserEntity;
 import com.lockbeck.entities.user.UserService;
-import com.lockbeck.exceptions.BadRequestException;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,13 +30,10 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 public class JsonController {
     public static final String DIRECTORY = System.getProperty("user.home")+"/Desktop/audit/jsons";
     private final JsonService jsonService;
-    private final JwtService jwtService;
-    private final UserService userService;
 
-    public JsonController(JsonService jsonService, JwtService jwtService, UserService userService) {
+    public JsonController(JsonService jsonService) {
         this.jsonService = jsonService;
-        this.jwtService = jwtService;
-        this.userService = userService;
+        
     }
 
     @PostMapping("/upload")
